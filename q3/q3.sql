29208aa4971c237e004902060a54b9039b929adf
Steven Nodalo <s.nodalo656@student.nu.edu>
2021-07-05 00:33:55 -0700

    q3 Completed

--- q3/q3.sql
+++ q3/q3.sql
@@ -11,4 +11,76 @@ SET @v7 = 'EE';
 SET @v8 = 'MAT';
 
 -- 3. List the names of students who have taken course v4 (crsCode).
-SELECT name FROM Student WHERE id IN (SELECT studId FROM Transcript WHERE crsCode = @v4);
+SELECT 
+    name
+FROM
+    Student
+WHERE
+    id IN (SELECT 
+            studId
+        FROM
+            Transcript
+        WHERE
+            crsCode = @v4);
+            
+-- OPTIMIZATION Step 1
+-- Analyze query using EXPLAIN and EXPLAIN ANALYZE
+
+EXPLAIN ANALYZE
+SELECT 
+    name
+FROM
+    Student
+WHERE
+    id IN (SELECT 
+            studId
+        FROM
+            Transcript
+        WHERE
+            crsCode = @v4);
+
+-- OPTIMIZATION Step 2
+-- Try using INNER JOIN
+
+EXPLAIN ANALYZE
+SELECT name
+FROM student s
+INNER JOIN transcript t
+ON s.id = t.studId
+WHERE crsCode = @v4;
+
+-- OPTIMIZATION Step 3
+-- Try creating indexes on student.id column and transcript.studId column
+
+CREATE INDEX index_id
+ON student (id);
+
+CREATE INDEX index_studId
+ON transcript (studId);
+
+
+-- OPTIMIZATION Step 4
+-- Analyze new query speed with indexes using EXPLAIN and EXPLAIN ANALYZE
+
+
+SELECT name
+FROM student s
+INNER JOIN transcript t
+ON s.id = t.studId
+WHERE crsCode = @v4;
+
+
+-- OPTIMIZATION Step 5
+-- Create index on crsCode column
+
+CREATE INDEX index_crsCode
+ON transcript (crsCode);
+
+-- OPTIMIZATION Step 6
+-- Analyze using EXPLAIN and EXPLAIN ANALYZE
+
+SELECT name
+FROM student s
+INNER JOIN transcript t
+ON s.id = t.studId
+WHERE crsCode = @v4;
